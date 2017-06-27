f01be9dce0768f31fbecd454883e9f0832650616
xiguangxingxiahuaxian <1787364567@qq.com>
2017-06-27 11:01:12 +0800

    first

---
+++ app/src/main/java/com/study/android/demo_2017_6_27/MainActivity.java
@@ -0,0 +1,13 @@
+package com.study.android.demo_2017_6_27;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
